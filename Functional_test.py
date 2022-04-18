6e048e6d40649e77763a6a1b407cce570867b911
Tiantian0107 <872281270@qq.com>
2022-04-18 14:07:15 +0800

    Second commit

--- Functional_test.py
+++ Functional_test.py
@@ -1,30 +1,23 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
 
-# Edith has heard about a cool new online to-do app. She goes
-# to check out its homepage
-browser.get('http://localhost:8000')
+class NewVisitorTest(unittest.TestCase):
 
-# she notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title, "Browser title was" +browser.title
-# She is invited to enter a to-do item straight away
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# She types "Buy peacock feathers" into a text box （Edith's hobby
-# is tying fly-fishing lures)
+    def tearDown(self):
+        self.browser.quit()
 
-# when she hits enter,the page updates,and how the page lists
-# '1' Buy peacock feathers" as an item in a to-do list
-# There is still a text-book inviting her to add another item.She
-# enters "Use peacock feathers" to make a fly" (Edith is very methodical)
-# The page updates again, and how shows both items on her list
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
 
-# Edith wonders whether the site will remember her list.Then she sees
-# that the site has generated a unique URL for her --there is some
-# explanatory text to that effect
+        # she notices the page title and header mention to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# She visits that URL -her to-do list is still there
-
-# Satisfied, she goes back to sleep
-
-browser.quit()
+if __name__ == '__main__':
+   unittest.main(warnings='ignore')
